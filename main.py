936da6f6462e354e459e467c01c158bf4445d313
tianist <gamer654@gmail.com>
2025-12-18 18:36:43 -0500

    More Game Logic Updates, Draws player on screen

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
         log_state()
+        screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
         clock.tick(60)
     pygame.quit()
