f5bb9bb9c4fbed7f5496a90ef86dd21ae5df7969
tianist <gamer654@gmail.com>
2025-12-18 19:19:49 -0500

    More Fixes, updateable, drawable groups

--- main.py
+++ main.py
@@ -4,21 +4,25 @@ from logger import log_state
 from player import Player
 
 def main():
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
-    player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+    dt = clock.tick(60) / 1000
     running = True
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        log_state()
         screen.fill((0, 0, 0))
-        player.draw(screen)
-        player.update(dt)
+        for entity in drawable:
+            entity.draw(screen)
+        updatable.update(dt)
+        log_state()
         pygame.display.flip()
-        dt = clock.tick(60)
     pygame.quit()
 
 if __name__ == "__main__":
