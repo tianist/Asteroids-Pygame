24f2487e67dd298aee87acea1ca0c5c3b056736b
tianist <gamer654@gmail.com>
2025-12-18 20:01:14 -0500

    ASTEROIDS SPAWN IN!

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 from player import Player
+from asteroidfield import AsteroidField
+from asteroid import Asteroid
 
 def main():
     pygame.init()
@@ -11,8 +13,12 @@ def main():
     running = True
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids,updatable, drawable)
     Player.containers = (updatable, drawable)
+    AsteroidField.containers = (updatable,)
     player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+    asteroidfield = AsteroidField()
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -23,6 +29,7 @@ def main():
         updatable.update(dt)
         log_state()
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
     pygame.quit()
 
 if __name__ == "__main__":
